b114c2461973495995a43ddac3470b62681fb1ca
Jan Dale Amores <amoresjan14@gmail.com>
2019-10-30 20:50:34 +0800

    added initial frontedn

---
+++ frontend/src/pages/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="container py-5 bg-black">
+			<p className="m-0 text-center text-white small">
+				Copyright &copy; Your Website 2019
+			</p>
+		</div>
+	);
+};
+
+export default Footer;
